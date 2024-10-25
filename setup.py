13274bbede03e1721dc82c6d8971a7608b344403
Keva1z <slavkaslavan@yandex.ru>
2024-10-26 00:46:59 +0400

    Updated requirements

--- setup.py
+++ setup.py
@@ -2,15 +2,29 @@ from setuptools import setup, find_packages
 
 setup(
     name="blackboxapi",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
-        "requests",
+        "requests>=2.32.0",
+        "aiohttp>=3.10.0",
+        "aiohappyeyeballs>=2.4.0",
+        "Brotli>=1.1.0",
+        "colorama>=0.4.6",
+        "propcache>=0.2.0",
+        "urllib3>=2.2.0"
     ],
+    python_requires=">=3.8",
     author="Keva1z",
     author_email="Keva1z@yandex.ru",
-    description="Библиотека для работы с Blackbox API",
+    description="Python library for Blackbox AI API integration",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Keva1z/blackboxapi",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
