9db55642fa269966792a035f4dfcc8fd2b2f6528
Keva1z <slavkaslavan@yandex.ru>
2024-11-20 21:04:36 +0400

    fix: bypassed validation

--- tests/async_client.py
+++ tests/async_client.py
@@ -1,16 +1,22 @@
+import sys
+import os
 import asyncio
+
+# Добавляем путь к исходному коду в PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from blackboxapi.client import AIClient
 from blackboxapi.agent import RU_CAN_CODER
 
 async def main():
-    client = AIClient(logging=True)
+    client = AIClient(enable_logging=True)
     agent_mode = RU_CAN_CODER
 
     while True:
         prompt = input(">> ")
         if prompt == "//": break
         
-        response = await client.completions.create_async(prompt, agent_mode, max_tokens=90000)
+        response = await client.completions.create_async(prompt, max_tokens=90000)
         print("Assistant:", response)
 
 asyncio.run(main())
